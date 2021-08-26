49a12c2a935f771c6c81503cef1b4e147946830f
MiseryD <80783487+MiseryD@users.noreply.github.com>
2021-08-26 13:20:13 +0200

    Add files via upload

---
+++ commands/liste.js
@@ -0,0 +1,26 @@
+const Discord = require("discord.js");
+const { MessageEmbed } = require("discord.js");
+const { Color } = require("../../config.js");
+
+module.exports = {
+  name: "liste",
+  aliases: [],
+  description: "liste des commandes",
+  usage: "Ping",
+  run: async (client, message, args) => {
+    //Start
+    message.delete();
+
+    const embed = new MessageEmbed()
+      .setColor(Color)
+      .setTitle("__**Liste de mes commandes**__")
+      .setDescription("Utilisez =help <nom de la commande> pour plus d'info sur les commandes")
+      .addField("**- Fun**", "``Avatar, Coinflip, Howgay, Meme, Rate, 8ball, Ascii, Choose, Hack, Randomnumber``")
+      .addField("**Moderation**", "``Clear, Mute, Unmute, Tempmute, Kick, Ban, Unban, Tempban, Warn, Warnings, ResetWarns``")
+      .addField("**Informations**", "``Help, Covid, Weather, Userinfo, Serverinfo, Ping``")
+
+    message.channel.send(embed);
+
+    //End
+  }
+};
