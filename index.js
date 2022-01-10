aca6b0b4321b4ff26cd65cf8f4a448bbc9468834
MiseryD <80783487+MiseryD@users.noreply.github.com>
2022-01-10 18:09:32 +0100

    Update index.js

--- index.js
+++ index.js
@@ -104,6 +104,28 @@ client.on('guildMemberRemove', (member) => {
   leaveChannelAg.send('**Good Bye** <@' + member.user.id + '> ! 🙂');
 });
 
+var time = msg.createdAt - old_msg[old_msg.length-1].createdAt // DURÉE ENTRE LES DEUX DERNIERS MESSAGES
+
+                if (time < 5000) {//SPAM 5 sec
+                    msg.member.roles.add('ID ROLE SPAM') //AJOUTE LE ROLE SPAM
+                    
+                    client.channels.cache.get('ID DE VOTRE CHANNEL DEBUG/ADMIN').send(`${msg.member} a été mute`) // MSG ADMINISTRATIOn
+                    msg.channel.send(`Calme toi ${msg.member} !! `) // MSG DE PREVENTION
+                    timer(msg,client)//DESACTIVÉ AU BOUT D'UN X LE SPAM
+                    msg.delete() // SUPPRIME LE MESSAGE
+                    
+                }
+
+            }else if(old_msg.length > 3){
+                old_msg = []
+            }
+
+        }catch(e){ // SI UNE ERREUR SURVIENT
+            console.error(e);
+        }
+
+        old_msg.push(msg)
+    })
 
 client.login(process.env.TOKEN);
 
